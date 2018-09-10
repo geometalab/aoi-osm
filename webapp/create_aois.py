ff8e2f172276376fe79c1bc47170a22d82dda9d6
ANGkeith <kang024@e.ntu.edu.sg>
2018-09-10 21:43:08 +0800

    add features to create_aois.py: additional argument to filter the result

--- webapp/create_aois.py
+++ webapp/create_aois.py
@@ -3,6 +3,7 @@ import argparse
 import psycopg2
 import time
 from app.aoi_query_generator import AoiQueryGenerator
+import geopandas as gpd
 
 
 def exec_sql(sql):
@@ -12,14 +13,30 @@ def exec_sql(sql):
         with connection.cursor() as cursor:
             cursor.execute(sql)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--output_geojson', required=True)
-parser.add_argument('--hull_algorithm', choices=['concave', 'convex'], default='convex')
+
+def is_valid_file(parser, arg):
+    try:
+        boundary_4326 = gpd.read_file(arg)
+        boundary_4326 = boundary_4326.to_crs({'init': 'epsg:4326'})
+        return boundary_4326
+    except (Exception) as e:
+        raise argparse.ArgumentTypeError(e) from e
+
+
+
+parser = argparse.ArgumentParser(description='Used to export the AOIs extract`')
+parser.add_argument('DEST', help='file path for the exported AOIs')
+parser.add_argument('--filter_boundary_path', default=None,
+                    help='filters the exported AOIs to those that intersects the boundary specified in this '
+                         'GeoJSON file',
+                    type=lambda x: is_valid_file(parser, x), metavar='PATH')
+parser.add_argument('--hull_algorithm', choices=['concave', 'convex'], default='convex',
+                        help='algorthim used to create the hull (cluster)')
 parser.add_argument('--with_network_centrality', action='store_true', default=False)
 args = parser.parse_args()
 
 start = time.time()
-aois_query_generator = AoiQueryGenerator(hull_algorithm=args.hull_algorithm)
+aois_query_generator = AoiQueryGenerator(hull_algorithm=args.hull_algorithm, boundary=args.filter_boundary_path)
 
 if args.with_network_centrality:
     aois_query = aois_query_generator.extended_hulls_query()
@@ -34,10 +51,10 @@ CREATE TABLE aois_with_network_centrality(hull geometry);
 INSERT INTO aois_with_network_centrality ({})
     """.format(aois_query))
 
-    check_call(["rm", "-f", args.output_geojson])
+    check_call(["rm", "-f", args.DEST])
     check_call(["ogr2ogr",
                 "-f", "GeoJSON",
-                args.output_geojson,
+                args.DEST,
                 "PG:host=postgres dbname=gis user=postgres",
                 "-sql", "select st_transform(hull, 4326) from aois_with_network_centrality"])
 else:
@@ -53,12 +70,12 @@ CREATE TABLE aois_without_network_centrality(hull geometry);
 INSERT INTO aois_without_network_centrality ({})
     """.format(aois_query))
 
-    check_call(["rm", "-f", args.output_geojson])
+    check_call(["rm", "-f", args.DEST])
     check_call(["ogr2ogr",
                 "-f", "GeoJSON",
-                args.output_geojson,
+                args.DEST,
                 "PG:host=postgres dbname=gis user=postgres",
                 "-sql", "select st_transform(hull, 4326) from aois_without_network_centrality"])
 
 
-print("creating aois took {}s".format(time.time() - start))
+print("Creating aois took {}s".format(time.time() - start))
