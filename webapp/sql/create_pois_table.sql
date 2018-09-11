c4caba82a2a60894e65380f29edfb2dbbd03b31a
ANGkeith <kang024@e.ntu.edu.sg>
2018-09-11 19:31:51 +0800

    retab

--- webapp/sql/create_pois_table.sql
+++ webapp/sql/create_pois_table.sql
@@ -1,51 +1,51 @@
 DROP TABLE IF EXISTS pois;
 
 CREATE TABLE pois (
-	id SERIAL PRIMARY KEY,
-	geometry geometry,
+    id SERIAL PRIMARY KEY,
+    geometry geometry,
 );
 
 -- Note that building filters some pois such as sports_centre because sports 
 -- centre is not a building
 CREATE OR REPLACE VIEW building AS
-	SELECT *
-	FROM planet_osm_polygon AS building
-	WHERE (
-		building IS NOT NULL
-		AND access IS DISTINCT FROM 'private'
-	);
-		
+    SELECT *
+    FROM planet_osm_polygon AS building
+    WHERE (
+        building IS NOT NULL
+        AND access IS DISTINCT FROM 'private'
+    );
+        
 INSERT INTO pois(geometry) (
-	SELECT way
-	FROM (
-		SELECT
-			CASE 
-				WHEN building.way IS NULL THEN point.way
-				ELSE building.way
-			END, point.amenity, point.shop, point.leisure, 
-			point.landuse, point.access
-		FROM building RIGHT JOIN planet_osm_point AS point 
-		ON ST_Within(point.way, building.way)
-		
-		UNION ALL
-		
-		SELECT way AS geometry, amenity, shop, leisure, landuse, "access"
-		-- Note that building filters some pois such as sports_centre 
-		-- because sports centre is  not a building that's why 
-		-- planet_osm_polygon is used instead
-		FROM planet_osm_polygon
+    SELECT way
+    FROM (
+        SELECT
+            CASE 
+                WHEN building.way IS NULL THEN point.way
+                ELSE building.way
+            END, point.amenity, point.shop, point.leisure, 
+            point.landuse, point.access
+        FROM building RIGHT JOIN planet_osm_point AS point 
+        ON ST_Within(point.way, building.way)
+        
+        UNION ALL
+        
+        SELECT way AS geometry, amenity, shop, leisure, landuse, "access"
+        -- Note that building filters some pois such as sports_centre 
+        -- because sports centre is  not a building that's why 
+        -- planet_osm_polygon is used instead
+        FROM planet_osm_polygon
 
-	)AS pois_subquery
-	
-	WHERE (
-		amenity = ANY(ARRAY['pub', 'bar', 'cafe', 'restaurant',
-		'pharmacy', 'bank', 'fast_food', 'food_court', 'ice_cream',
-		'library', 'ferry_terminal', 'pharmacy', 'arts_centre', 'cinema',
-		'community_centre', 'casino', 'fountain', 'nightclub', 'studio',
-		'theatre', 'internet_cafe', 'marketplace', 'post_office',
-		'townhall'])
+    )AS pois_subquery
+    
+    WHERE (
+        amenity = ANY(ARRAY['pub', 'bar', 'cafe', 'restaurant',
+        'pharmacy', 'bank', 'fast_food', 'food_court', 'ice_cream',
+        'library', 'ferry_terminal', 'pharmacy', 'arts_centre', 'cinema',
+        'community_centre', 'casino', 'fountain', 'nightclub', 'studio',
+        'theatre', 'internet_cafe', 'marketplace', 'post_office',
+        'townhall'])
 
-		OR shop = ANY(ARRAY['mall', 'bakery', 'beverages', 'butcher',
+        OR shop = ANY(ARRAY['mall', 'bakery', 'beverages', 'butcher',
                 'chocolate', 'coffee', 'confectionery', 'deli', 'frozen_food',
                 'greengrocer', 'healthfood', 'ice_cream', 'pasta', 'pastry',
                 'seafood', 'spices', 'tea', 'department_store', 'supermarket',
@@ -56,14 +56,14 @@ INSERT INTO pois(geometry) (
                 'music', 'books', 'gift', 'stationery', 'ticket', 'laundry',
                 'pet', 'tobacco', 'toys'])
 
-		OR leisure = ANY(ARRAY['adult_gaming_centre',
-		'amusement_arcade', 'beach_resort', 'fitness_centre',
-		'ice_rink', 'sports_centre', 'water_park'])
+        OR leisure = ANY(ARRAY['adult_gaming_centre',
+        'amusement_arcade', 'beach_resort', 'fitness_centre',
+        'ice_rink', 'sports_centre', 'water_park'])
 
-		OR landuse = ANY(ARRAY['retail'])
-	) 
-	
-	AND "access" IS DISTINCT FROM 'private'
+        OR landuse = ANY(ARRAY['retail'])
+    ) 
+    
+    AND "access" IS DISTINCT FROM 'private'
 );
 
 CREATE INDEX pois_geometry ON pois USING gist(geometry);
