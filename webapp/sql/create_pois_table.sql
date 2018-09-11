1cad24560de92f908d89e410bd453138b2ce45fe
ANGkeith <kang024@e.ntu.edu.sg>
2018-09-11 19:49:23 +0800

    rename building to public_building & added in missing the ST_Buffer

--- webapp/sql/create_pois_table.sql
+++ webapp/sql/create_pois_table.sql
@@ -2,14 +2,14 @@ DROP TABLE IF EXISTS pois;
 
 CREATE TABLE pois (
     id SERIAL PRIMARY KEY,
-    geometry geometry,
+    geometry geometry
 );
 
--- Note that building filters some pois such as sports_centre because sports 
--- centre is not a building
-CREATE OR REPLACE VIEW building AS
+-- Note that public_building filters some pois such as sports_centre because 
+-- sports centre is not a public_building
+CREATE OR REPLACE VIEW public_building AS
     SELECT *
-    FROM planet_osm_polygon AS building
+    FROM planet_osm_polygon AS public_building
     WHERE (
         building IS NOT NULL
         AND access IS DISTINCT FROM 'private'
@@ -20,18 +20,18 @@ INSERT INTO pois(geometry) (
     FROM (
         SELECT
             CASE 
-                WHEN building.way IS NULL THEN point.way
-                ELSE building.way
-            END, point.amenity, point.shop, point.leisure, 
-            point.landuse, point.access
-        FROM building RIGHT JOIN planet_osm_point AS point 
-        ON ST_Within(point.way, building.way)
+                WHEN public_building.way IS NULL THEN ST_Buffer(point.way, 5, 1)
+                ELSE public_building.way
+            END, point.amenity, point.shop, point.leisure, point.landuse, 
+                point.access
+        FROM public_building RIGHT JOIN planet_osm_point AS point 
+        ON ST_Within(point.way, public_building.way)
         
         UNION ALL
         
         SELECT way AS geometry, amenity, shop, leisure, landuse, "access"
-        -- Note that building filters some pois such as sports_centre 
-        -- because sports centre is  not a building that's why 
+        -- Note that public_building filters some pois such as sports_centre 
+        -- because sports centre is  not a public_building that's why 
         -- planet_osm_polygon is used instead
         FROM planet_osm_polygon
 
@@ -39,26 +39,26 @@ INSERT INTO pois(geometry) (
     
     WHERE (
         amenity = ANY(ARRAY['pub', 'bar', 'cafe', 'restaurant',
-        'pharmacy', 'bank', 'fast_food', 'food_court', 'ice_cream',
-        'library', 'ferry_terminal', 'pharmacy', 'arts_centre', 'cinema',
-        'community_centre', 'casino', 'fountain', 'nightclub', 'studio',
-        'theatre', 'internet_cafe', 'marketplace', 'post_office',
-        'townhall'])
+            'pharmacy', 'bank', 'fast_food', 'food_court', 'ice_cream',
+            'library', 'ferry_terminal', 'pharmacy', 'arts_centre', 'cinema',
+            'community_centre', 'casino', 'fountain', 'nightclub', 'studio',
+            'theatre', 'internet_cafe', 'marketplace', 'post_office',
+            'townhall'])
 
         OR shop = ANY(ARRAY['mall', 'bakery', 'beverages', 'butcher',
-                'chocolate', 'coffee', 'confectionery', 'deli', 'frozen_food',
-                'greengrocer', 'healthfood', 'ice_cream', 'pasta', 'pastry',
-                'seafood', 'spices', 'tea', 'department_store', 'supermarket',
-                'bag', 'boutique', 'clothes', 'fashion', 'jewelry', 'leather',
-                'shoes', 'tailor', 'watches', 'chemist', 'cosmetics',
-                'hairdresser', 'medical_supply', 'electrical', 'hardware',
-                'electronics', 'sports', 'swimming_pool', 'collector', 'games',
-                'music', 'books', 'gift', 'stationery', 'ticket', 'laundry',
-                'pet', 'tobacco', 'toys'])
+            'chocolate', 'coffee', 'confectionery', 'deli', 'frozen_food',
+            'greengrocer', 'healthfood', 'ice_cream', 'pasta', 'pastry',
+            'seafood', 'spices', 'tea', 'department_store', 'supermarket',
+            'bag', 'boutique', 'clothes', 'fashion', 'jewelry', 'leather',
+            'shoes', 'tailor', 'watches', 'chemist', 'cosmetics',
+            'hairdresser', 'medical_supply', 'electrical', 'hardware',
+            'electronics', 'sports', 'swimming_pool', 'collector', 'games',
+            'music', 'books', 'gift', 'stationery', 'ticket', 'laundry',
+            'pet', 'tobacco', 'toys'])
 
-        OR leisure = ANY(ARRAY['adult_gaming_centre',
-        'amusement_arcade', 'beach_resort', 'fitness_centre',
-        'ice_rink', 'sports_centre', 'water_park'])
+        OR leisure = ANY(ARRAY['adult_gaming_centre','amusement_arcade', 
+            'beach_resort', 'fitness_centre', 'ice_rink', 'sports_centre', 
+            'water_park'])
 
         OR landuse = ANY(ARRAY['retail'])
     ) 
