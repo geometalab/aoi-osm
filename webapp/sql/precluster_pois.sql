fc3023b9923978fa4664ddbe5092461ba0617952
ANGkeith <kang024@e.ntu.edu.sg>
2018-09-19 21:25:14 +0800

    precluster bug

--- webapp/sql/precluster_pois.sql
+++ webapp/sql/precluster_pois.sql
@@ -14,7 +14,7 @@ INSERT INTO preclusters(hull, area, pois_count) (
   SELECT ST_ConvexHull(ST_Union(geometry)),
          ST_Area(ST_ConvexHull(ST_Union(geometry))),
          COUNT(geometry)
-  FROM clustered_pois AS hull WHERE cid > 0 GROUP BY cid
+  FROM clustered_pois AS hull WHERE cid >= 0 GROUP BY cid
 );
 
 UPDATE preclusters SET dbscan_minPts = GREATEST(2, round((-5.342775355 * 10^(-7) * area + 5.738819175 * 10^(-3) * pois_count + 2.912834423)));
